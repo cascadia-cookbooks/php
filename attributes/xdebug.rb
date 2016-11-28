ec29b2b156c4817e90470e6dd514403f576ac4fb
Matthew Davis <mattd@copiousinc.com>
2016-11-28 13:19:16 -0800

    Update attributes. Bump version.

--- attributes/xdebug.rb
+++ attributes/xdebug.rb
@@ -1,4 +1,20 @@
 default['php']['xdebug']['enabled'] = false
 default['php']['xdebug']['package'] = 'php-xdebug'
 default['php']['xdebug']['config_file'] = '/etc/php/7.0/mods-available/xdebug.ini'
-default['php']['xdebug']['directives'] = {}
+default['php']['xdebug']['directives'] = {
+    :auto_trace => '0',
+    :cli_color => '1',
+    :max_nesting_level => '256',
+    :profiler_enable => '0',
+    :profiler_enable_trigger => '1',
+    :profiler_output_name => 'cachegrind.out.%p.%H%R.%t',
+    :remote_autostart => '0',
+    :remote_connect_back => '1',
+    :remote_enable => '1',
+    :scream => '0',
+    :show_mem_delta => '1',
+    :trace_enable_trigger => '1',
+    :trace_format => '0',
+    :trace_options => '0',
+    :trace_output_name => 'trace.%H%R.%t'
+}
