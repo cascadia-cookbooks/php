fcac290cbd9663d3c43fd6d6a63986209d5a0bdf
Colby Olson <me@colbyolson.com>
2016-12-14 21:28:31 -0800

    bump to 0.6.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 license             'MIT'
 maintainer          'Copious, Inc.'
 maintainer_email    'engineering@copiousinc.com'
-version             '0.6.2'
+version             '0.6.3'
 source_url          'https://github.com/copious-cookbooks/php'
 issues_url          'https://github.com/copious-cookbooks/php/issues'
 
